4a21eb509fb9c800c301775e44582b2ed7d4e79d
root <root@test2.mec.ac.in>
2019-10-09 10:59:14 +0530

    removed db pass

--- include/secure/inc_constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-// DB Constants
-define("DB_SERVER_NAME", "localhost");
-define("DB_NAME", "institution");
-define("DB_USER", "root");
-//define("DB_PASSWORD", "student@mysql");
-define("DB_PASSWORD", "africanmoon");
-?>
